368ac9722e27b42601dab5b029809e49ac3a57ea
lauren kernan <lauren.kernan17@gmail.com>
2019-01-14 17:59:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 /* Enter the code to remove the main node element under this comment */
-
+main.remove()
 /* Create your new element here and assign it to newHeader */
-const newHeader = null;
+
+var newHeader = document.createElement('h1')
+document.body.appendChild(newHeader)
+newHeader.innerHTML="Lauren is the champion!"
+newHeader.id = "victory"
